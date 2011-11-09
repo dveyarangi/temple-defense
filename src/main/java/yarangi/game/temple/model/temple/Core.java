7bc055f7da82525dd933fff3f73dd6db5a4a7361
dveyarangi <chukchac@yahoo.com>
2011-11-09 20:16:34 +0200

    Added some dummy game elements

---
+++ src/main/java/yarangi/game/temple/model/temple/Core.java
@@ -0,0 +1,9 @@
+package yarangi.game.temple.model.temple;
+
+import yarangi.graphics.quadraturin.objects.Entity;
+import yarangi.graphics.quadraturin.objects.IEntity;
+
+public class Core extends Entity 
+{
+
+}
