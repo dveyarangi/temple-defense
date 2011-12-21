12c3621c7244a5a3befaa8d46475aacb13b82e6a
dveyarangi <chukchac@yahoo.com>
2011-12-21 11:20:09 +0200

    boids improved

--- src/main/java/yarangi/game/harmonium/enemies/swarm/BoidBehavior.java
+++ src/main/java/yarangi/game/harmonium/enemies/swarm/BoidBehavior.java
@@ -2,46 +2,63 @@ package yarangi.game.harmonium.enemies.swarm;
 
 import java.util.Set;
 
+import yarangi.game.harmonium.enemies.swarm.agents.DroneBehavior;
 import yarangi.game.harmonium.enemies.swarm.agents.SwarmAgent;
 import yarangi.graphics.quadraturin.objects.Entity;
 import yarangi.graphics.quadraturin.objects.ISensor;
 import yarangi.graphics.quadraturin.objects.behaviors.IBehaviorState;
+import yarangi.math.Geometry;
 import yarangi.math.Vector2D;
 
 public class BoidBehavior implements IBehaviorState<SwarmAgent> 
 {
-	public static final double CROWDING_COEF = 1;
-	public static final double SPREADING_COEF = 1;
-	public static final double FLOCKING_COEF = 1;
+	public static final double CROWDING_COEF = 0.6;
+	public static final double SPREADING_COEF = 0.5;
+	public static final double FLOCKING_COEF = 1.6;
+	
+	private DroneBehavior droning = new DroneBehavior(1);
 ///	public static final double FLOCKING_COEF = 1;
 	@Override
 	public double behave(double time, SwarmAgent boid) {
+		
+//		droning.behave( time, boid);
+		
 		ISensor<Entity> sensor = boid.getSensor();
 		
 		Set <Entity> neighbours = sensor.getEntities();
+		if(neighbours.size() == 1)
+			return 0;
 		
 		Vector2D loc = boid.getArea().getRefPoint();
 		
 		Vector2D massCenter = Vector2D.ZERO();
-		Vector2D spreadingForce = Vector2D.ZERO();
-		Vector2D flockingForce = Vector2D.ZERO();
+		Vector2D spreadingVelocity = Vector2D.ZERO();
+		Vector2D flockingVelocity = Vector2D.ZERO();
 		Vector2D otherLoc;
+		
+		double distance;
 		for(Entity entity : neighbours)
 		{
 			if(entity == boid)
 				continue;
+			
 			otherLoc = entity.getArea().getRefPoint();
+			distance = Geometry.calcHypot( loc, otherLoc );
+			
 			massCenter.add(otherLoc);
-			spreadingForce.add(otherLoc.minus(loc));
-			flockingForce.add(entity.getBody().getForce());
+			spreadingVelocity.substract(otherLoc.minus(loc).multiply(distance));
+			flockingVelocity.add(entity.getBody().getVelocity());
 		}
 		
 		massCenter.multiply(1./(neighbours.size()-1));
-		Vector2D crowdingForce = massCenter.substract(loc).multiply(CROWDING_COEF);
-		spreadingForce.mul(SPREADING_COEF);
-		flockingForce.multiply(1./(neighbours.size()-1));
+		Vector2D crowdingVelocity = massCenter.substract(loc).multiply(CROWDING_COEF);
+		flockingVelocity.multiply(1./(neighbours.size()-1));
+		flockingVelocity.substract( boid.getBody().getVelocity() );
+		flockingVelocity.multiply(FLOCKING_COEF);
 		
-		boid.getBody().setForce(crowdingForce.plus(spreadingForce).plus(flockingForce));
+		spreadingVelocity.multiply( 1./(neighbours.size()-1)*SPREADING_COEF );
+		
+		boid.getBody().setForce( crowdingVelocity.plus(spreadingVelocity).plus(flockingVelocity));
 		
 		return 0;
 	}
